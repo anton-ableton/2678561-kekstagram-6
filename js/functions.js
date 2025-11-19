dd2b8d728874182614c7493ecf83591de23f9235
Anton Nazarov <a.nazarov3@g.nsu.ru>
2025-11-19 20:14:05 +0700

    remove console logs

--- js/functions.js
+++ js/functions.js
@@ -32,20 +32,20 @@ function getNumber(input) {
   return result === '' ? NaN : Number(result);
 }
 
-console.log(checkLength('проверяемая строка', 20));      // true
-console.log(checkLength('проверяемая строка', 18));      // true
-console.log(checkLength('проверяемая строка', 10));      // false
+checkLength('проверяемая строка', 20);      // true
+checkLength('проверяемая строка', 18);      // true
+checkLength('проверяемая строка', 10);      // false
 
-console.log(isPalindrome('топот'));                      // true
-console.log(isPalindrome('ДовОд'));                      // true
-console.log(isPalindrome('Кекс'));                       // false
-console.log(isPalindrome('Лёша на полке клопа нашёл ')); // true
+isPalindrome('топот');                      // true
+isPalindrome('ДовОд');                      // true
+isPalindrome('Кекс');                       // false
+isPalindrome('Лёша на полке клопа нашёл '); // true
 
-console.log(getNumber('2023 год'));                      // 2023
-console.log(getNumber('ECMAScript 2022'));               // 2022
-console.log(getNumber('1 кефир, 0.5 батона'));           // 105
-console.log(getNumber('агент 007'));                     // 7
-console.log(getNumber('а я томат'));                     // NaN
-console.log(getNumber(2023));                            // 2023
-console.log(getNumber(-1));                              // 1
-console.log(getNumber(1.5));                             // 15
+getNumber('2023 год');                      // 2023
+getNumber('ECMAScript 2022');               // 2022
+getNumber('1 кефир, 0.5 батона');           // 105
+getNumber('агент 007');                     // 7
+getNumber('а я томат');                     // NaN
+getNumber(2023);                            // 2023
+getNumber(-1);                              // 1
+getNumber(1.5);                             // 15
